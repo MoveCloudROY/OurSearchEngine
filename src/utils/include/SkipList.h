577352b31464f54d7f0606cc5ce55b7fe9468d23
roy1994 <dev.mancitrus@outlook.com>
2023-05-17 17:53:21 +0800

    (feat): complete the skiplist

--- src/utils/include/SkipList.h
+++ src/utils/include/SkipList.h
@@ -1,33 +1,34 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
-#include <json/value.h>
+#include <functional>
+#include <iostream>
+#include <json/reader.h>
 #include <memory>
+#include <random>
+#include <string>
 #include <type_traits>
 #include <utility>
 #include <vector>
-#include <random>
-#include <functional>
+
 #include <json/json.h>
-
-#include <iostream>
-
+#include <json/value.h>
 
 namespace SG {
 namespace Core {
 
+template <typename, typename = void>
+constexpr bool ValueCheck = false;
+
+// clang-format off
 template <typename T>
-class IsEqualityComparable {
-private:
-    template <typename K>
-    static constexpr auto Test(K*) -> decltype(std::declval<K>() == std::declval<K>());
-
-    template <typename K>
-    static constexpr int Test(...);
-
-public:
-    static constexpr bool Value = std::is_same_v<bool, decltype(Test<T>(nullptr))>;
-};
-
+constexpr bool ValueCheck<T, std::void_t<decltype(
+    std::declval<T>() < (std::declval<T>()),
+    std::declval<T>() == (std::declval<T>()),
+    (void)std::declval<T*>()->toJson(std::declval<const T&>()),
+    (void)std::declval<T*>()->fromJson(std::declval<const Json::Value&>())
+)>> = true;
+// clang-format on
 
 /*
     FST -> SkipList
@@ -41,19 +42,47 @@ public:
     head -> Data1 -> Data2 -> Data3 -> Data4 -> Data5 -> Data7
 
 */
-template <typename TK, typename TV, typename std::enable_if_t<IsEqualityComparable<TV>::Value, bool> = true>
+template <typename TV, typename std::enable_if_t<ValueCheck<TV>, bool> = true>
 class SkipList {
     struct Node;
 
 public:
     SkipList()
-        : m_maxLevel{1}, m_levels{0}, m_length{0}, m_gen{std::random_device{}()}, m_distr{} {
+        : m_maxLevel{1}
+        , m_levels{0}
+        , m_length{0}
+        , m_gen{std::random_device{}()}
+        , m_distr{} {
 
         head = new Node{nullptr, nullptr, nullptr, nullptr, 0, TV{}};
         createNewLevel(1);
         bottom = head->down;
     }
-    ~SkipList() {
+
+    /*
+        从 Json 构建 SkipList
+    */
+    SkipList(const Json::Value &src)
+        : m_maxLevel{src["levels"].asUInt64()}
+        , m_levels{0}
+        , m_length{src["length"].asUInt64()}
+        , m_gen{std::random_device{}()}
+        , m_distr{} {
+
+        head = new Node{nullptr, nullptr, nullptr, nullptr, 0, TV{}};
+        createNewLevel(1);
+        bottom = head->down;
+        createNewLevel(m_maxLevel);
+
+        const Json::Value &data = src["data"];
+        for (Json::Value::ArrayIndex i = 0; i != data.size(); ++i) {
+            TV          tmp   = TV::fromJson(data[i]);
+            std::size_t level = data[i]["level"].asUInt64();
+            insertNodes(tmp, level);
+        }
+    }
+
+    ~SkipList() noexcept {
         auto p = bottom;
 
         while (p) {
@@ -68,9 +97,9 @@ public:
     }
 
     /*
-        返回小于 value 的最大的 Node
-    */
-    Node* search(const TV& value) {
+    返回小于 value 的最大的 Node
+*/
+    Node *search(const TV &value) const {
         auto p = head;
         while (p->down) {
             p = p->down;
@@ -81,58 +110,52 @@ public:
         return p;
     }
 
+    Node *front() { return bottom->right; }
+
     /*
-    Firstly:
+Firstly:
 
-        head0     =>    head0           =>      head0
-                          |                      |
-                        head -> null            
+    head0     =>    head0           =>      head0
+                      |                      |
+                    head -> null
 
-    Typically:
+Typically:
 
-        head0
-          |
-        head -> Data1 ---------> (Data3) ----xxxxx------------> (Data7) -> null
-        head -> Data1 -> Data2 -> Data3 -> ([Data4]) -> Data5 -> Data7  -> null
-                                    <<<<<<<<<<  
-    */
+    head0
+      |
+    head -> Data1 ---------> (Data3) ----xxxxx------------> (Data7) -> null
+    head -> Data1 -> Data2 -> Data3 -> ([Data4]) -> Data5 -> Data7  -> null
+                                <<<<<<<<<<
+*/
 
-    void insert(const TV& value) {
+    void insert(const TV &value) {
         // 小于 value 的最大节点
-        auto  p        = search(value);
-        Node* posIndex = nullptr;
-
         auto nodeLevel = getRandomLevel();
         createNewLevel(nodeLevel);
-        for (int i = 0; i < nodeLevel; ++i) {
-            // 最底层时
-            if (posIndex != nullptr) {
-                while (!p->up)
-                    p = p->left;
-                p = p->up;
-            }
-            posIndex = insertNode(p, posIndex, value, nodeLevel);
-            p        = posIndex;
-        }
+
+        insertNodes(value, nodeLevel);
+
         ++m_length;
         expandLevel();
     }
 
     template <typename... Args>
-    void emplace_insert(Args&&... args) {
+    void emplace_insert(Args &&...args) {
         insert(TV{std::forward<Args>(args)...});
     }
-
-    Json::Value dump(std::function<Json::Value(const TV& v)> func) {
+    /*
+    序列化为 Json
+    */
+    Json::Value dump() const {
         Json::Value sklRt;
         Json::Value nodes;
 
         auto p = bottom;
         p      = p->right;
 
-        uint32_t cnt = 0;
+        std::size_t cnt = 0;
         while (p) {
-            Json::Value node = func(p->value);
+            Json::Value node = TV::toJson(p->value);
             node["rank"]     = ++cnt;
             node["level"]    = p->level;
             nodes.append(node);
@@ -145,14 +168,12 @@ public:
         return sklRt;
     }
 
-    void read() {}
-
     void print() const {
         for (int i = 0; i < m_maxLevel; ++i) {
             std::cout << "   ****   ";
         }
         std::cout << std::endl;
-        Node* p = bottom;
+        Node *p = bottom;
 
         while (p->right) {
             p      = p->right;
@@ -163,21 +184,71 @@ public:
             }
             std::cout << std::endl;
         }
+        std::cout << "==================" << std::endl;
+    }
+
+    static std::vector<TV> combine(std::vector<SkipList<TV> *> &v) {
+        std::vector<TV> ret;
+        if (v.empty())
+            return ret;
+        std::size_t okCnt       = 0;
+        Node       *currentNode = v[0]->front();
+
+        auto i = v.begin();
+        while (true) {
+            if (currentNode == nullptr)
+                break;
+            Node *prevNode       = (*i)->search(currentNode->value);
+            Node *bigOrEqualNode = prevNode->right;
+            if (bigOrEqualNode == nullptr)
+                break;
+            if (bigOrEqualNode->value == currentNode->value) {
+                okCnt++;
+                if (okCnt == v.size()) {
+                    okCnt = 0;
+                    ret.emplace_back(currentNode->value);
+                    currentNode = bigOrEqualNode->right;
+                }
+            } else {
+                okCnt       = 0;
+                currentNode = bigOrEqualNode;
+            }
+
+            i++;
+            if (i == v.end())
+                i = v.begin();
+        }
+        return ret;
     }
 
 private:
-    void createNewLevel(uint32_t level) {
+    void createNewLevel(std::size_t level) {
         while (m_levels < level) {
-            Node* tmp = new Node{nullptr, nullptr, head, head->down, 0, TV{}};
+            Node *tmp = new Node{nullptr, nullptr, head, head->down, 0, TV{}};
             if (head->down)
                 head->down->up = tmp;
             head->down = tmp;
             ++m_levels;
         }
     }
+    void insertNodes(const TV &value, std::size_t level) {
+        auto  p        = search(value);
+        Node *posIndex = nullptr;
 
-    Node* insertNode(Node* posLeft, Node* posDown, const TV& value, uint32_t level) {
-        Node* tmp = new Node{posLeft, posLeft->right, nullptr, posDown, level, value};
+        for (int i = 0; i < level; ++i) {
+            // 中间层时
+            if (posIndex != nullptr) {
+                while (!p->up)
+                    p = p->left;
+                p = p->up;
+            }
+            posIndex = insertNode(p, posIndex, value, level);
+            p        = posIndex;
+        }
+    }
+    Node *insertNode(Node *posLeft, Node *posDown, const TV &value, std::size_t level) {
+        Node *tmp =
+            new Node{posLeft, posLeft->right, nullptr, posDown, level, value};
         if (posLeft->right)
             posLeft->right->left = tmp;
         posLeft->right = tmp;
@@ -187,13 +258,12 @@ private:
         return tmp;
     }
     void expandLevel() {
-        std::cout << m_maxLevel << ' ' << m_length << '\n';
-        if ((1 << m_maxLevel) < m_length)
-            m_maxLevel = static_cast<uint32_t>(1.6 * m_maxLevel + 0.5);
+        if (static_cast<std::size_t>(1.5 * m_maxLevel + 0.5) < m_length)
+            m_maxLevel = static_cast<std::size_t>(1.5 * m_maxLevel + 0.5);
     }
 
-    uint32_t getRandomLevel() {
-        uint32_t ret = 1;
+    std::size_t getRandomLevel() {
+        std::size_t ret = 1;
         while (m_distr(m_gen) < THRED && ret < m_maxLevel) {
             ret++;
         }
@@ -204,14 +274,14 @@ private:
     static constexpr double THRED = 0.5;
 
     struct Node {
-        Node *   left, *right, *up, *down;
-        uint32_t level;
-        TV       value;
+        Node       *left, *right, *up, *down;
+        std::size_t level;
+        TV          value;
     };
-    uint32_t                         m_maxLevel;
-    Node *                           head, *bottom;
-    uint32_t                         m_levels;
-    uint32_t                         m_length;
+    std::size_t                      m_maxLevel;
+    Node                            *head, *bottom;
+    std::size_t                      m_levels;
+    std::size_t                      m_length;
     std::mt19937                     m_gen;
     std::uniform_real_distribution<> m_distr;
 };
